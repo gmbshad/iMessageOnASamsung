fc525ba0d853470aab33a6fe539e25950ec45123
gmbshad <96317696+gmbshad@users.noreply.github.com>
2022-06-10 02:46:17 -0600

    Recompiled Samsung Messages with the src code for BlueBubbles
    Just so i can run my business more smoothly... I'm always on my mac but despise the iPhone as my daily phone.
    
    With this I can receive and respond messages on my Note 10+ while my iPhone is just a paperweight with a sim card in it -- ONLY because I can answer it on my Mac and iPad as well. I run my own business and got tired of missing calls!

---
+++ iMessageOnASamsung/build/generated/source/buildConfig/release/com/example/imessageonasamsung/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.imessageonasamsung;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.example.imessageonasamsung";
+  public static final String BUILD_TYPE = "release";
+}
